2273d490712a540a01afcd8faddc0515d1af6438
Isaac Lyman <isaaclyman@gmail.com>
2018-08-10 15:10:17 -0600

    First commit

---
+++ app/src/main/java/com/isaaclyman/humanbingo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.isaaclyman.humanbingo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
