3b717f1a937ad82cb2977f3b9d8c659a7a17451a
Isaac Lyman <isaaclyman@gmail.com>
2018-08-10 18:06:30 -0600

    Functionality for New Game/Enter Code pages

--- app/src/main/java/com/isaaclyman/humanbingo/EnterCode.kt
+++ app/src/main/java/com/isaaclyman/humanbingo/EnterCode.kt
@@ -1,13 +1,43 @@
 package com.isaaclyman.humanbingo
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class EnterCode : AppCompatActivity() {
+
+    private var codeTextBox: EditText? = null
+    private var startGame: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter_code)
-    }
 
+        codeTextBox = findViewById(R.id.codeTextBox)
+        codeTextBox?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (codeTextBox?.text.toString().trim().length > 0) {
+                    startGame?.isClickable = true
+                } else {
+                    startGame?.isClickable = false
+                }
+            }
+        })
+
+        startGame = findViewById(R.id.startGameWithCode)
+        startGame?.setOnClickListener(View.OnClickListener { _ ->
+            val startGame = Intent(applicationContext, GameScreen::class.java)
+            startGame.putExtra("gameCode", codeTextBox?.text.toString())
+            startActivity(startGame)
+        })
+    }
 }
--- app/src/main/java/com/isaaclyman/humanbingo/NewGame.kt
+++ app/src/main/java/com/isaaclyman/humanbingo/NewGame.kt
@@ -1,13 +1,48 @@
 package com.isaaclyman.humanbingo
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
 
 class NewGame : AppCompatActivity() {
+    private var gameModeRadioGroup: RadioGroup? = null
+    private var startNewGame: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_game)
+
+        gameModeRadioGroup = findViewById(R.id.gameModeRadioGroup)
+        gameModeRadioGroup?.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener { group, checkedId ->
+            if (checkedId != -1) {
+                startNewGame?.isClickable = true
+            } else {
+                startNewGame?.isClickable = false
+            }
+        })
+
+        startNewGame = findViewById(R.id.startNewGame)
+        startNewGame?.setOnClickListener(View.OnClickListener { _ ->
+            val startGame = Intent(applicationContext, GameScreen::class.java)
+            startGame.putExtra("gameType", getGameMode())
+        })
     }
 
+    private fun getGameMode(): Int {
+        val checkedId = gameModeRadioGroup?.checkedRadioButtonId
+        val three = findViewById<RadioButton>(R.id.threeGameRadio).id
+        val four = findViewById<RadioButton>(R.id.fourGameRadio).id
+        val five = findViewById<RadioButton>(R.id.fiveGameRadio).id
+
+        return when (checkedId) {
+            three -> return 3
+            four -> return 4
+            five -> return 5
+            else -> throw Error("Invalid game mode")
+        }
+    }
 }
